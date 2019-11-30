67f7d382b5964065d507b070bff75efe910ecd93
Imanda <Imanda@github.com>
2019-11-30 09:36:37 +0700

    Merge from Reyhan's Branch, Script CameraMovement and Movement
    With stop method added for narration purpose. So, it will make character and camera stop when player trigger narration from fungus

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    // Limit the player movement
+    public float xPosLim;
+    public float xNegLim;
+    
+    private bool isMove = false;
+    private bool isFungusShows = false;
+    private float target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            if (!isMove) isMove = true;
+        }
+        if (isMove && !isFungusShows)
+        {
+            Vector3 pos = transform.position;
+            if (target >= xPosLim) target = xPosLim;
+            if (target <= xNegLim) target = xNegLim;
+            pos.x = Mathf.MoveTowards(transform.position.x, target, speed * Time.deltaTime);
+            transform.position = pos;
+        }
+    }
+
+    void SetPlayerAvailability()
+    {
+        if (!isFungusShows) isFungusShows = true;
+        else isFungusShows = false;
+    }
+}
