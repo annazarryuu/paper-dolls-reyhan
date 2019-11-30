eafadcbabdbd591862d92839549eb8620360bf37
Han <mreyhan693@gmail.com>
2019-11-30 09:51:08 +0700

    Trying to fixing bugs in Cam

--- Assets/CamMovement.cs
+++ Assets/CamMovement.cs
@@ -16,11 +16,13 @@ public class CamMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 initPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (!isFungusShows)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.Log("A: " + startPos);
             }
             if (Input.GetMouseButton(0))
             {
@@ -28,6 +30,7 @@ public class CamMovement : MonoBehaviour
                 Vector3 pos = Camera.main.transform.position;
                 pos.x += direction.x;
                 Camera.main.transform.position = pos;
+               // Debug.Log("B: " + Camera.main.transform.pos);
             }
         }
     }
