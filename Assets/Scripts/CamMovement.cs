12c6d117c74a2d258dd5ca4e031e440c46bfe8fd
Han <mreyhan693@gmail.com>
2019-11-30 15:09:59 +0700

    Limiting Player and Camera Movement

--- Assets/Scripts/CamMovement.cs
+++ Assets/Scripts/CamMovement.cs
@@ -6,11 +6,24 @@ public class CamMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 startPos;
-
+    public float xLeftMost;
+    public float xRightMost;
     private bool isFungusShows = false;
+    private float minArea;
+    private float maxArea;
+
+    private float camHeight;
+    private float camWidth;
+
+    [SerializeField]
+    GameObject environment;
     void Start()
     {
-        
+        minArea = environment.GetComponent<SpriteRenderer>().bounds.min.x;
+        maxArea = environment.GetComponent<SpriteRenderer>().bounds.max.x;
+
+        camHeight = 2f * Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -28,6 +41,8 @@ public class CamMovement : MonoBehaviour
                 Vector3 direction = startPos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector3 pos = Camera.main.transform.position;
                 pos.x += direction.x;
+                if (pos.x - camWidth / 2 <= minArea) pos.x = minArea + camWidth/2;
+                if (pos.x + camWidth / 2 >= maxArea) pos.x = maxArea - camWidth/2;
                 Camera.main.transform.position = pos;
                // Debug.Log("B: " + Camera.main.transform.pos);
             }
