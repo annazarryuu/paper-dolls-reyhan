d18efc048655a917756be863f7b7440769bf2502
Imanda <Imanda@github.com>
2019-11-30 12:06:53 +0700

    Add animation for player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,15 +9,20 @@ public class Movement : MonoBehaviour
     // Limit the player movement
     public float xPosLim;
     public float xNegLim;
-    
+
+	private Animator anim;
     private bool isMove = false;
     private bool isFungusShows = false;
     private float target;
-    // Start is called before the first frame update
-    void Start()
+	private Vector3 faceRight;
+	private Vector3 faceLeft;
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		anim = GetComponent<Animator>();
+		faceLeft = new Vector3(-0.485f, 0.485f, 0.485f);
+		faceRight = new Vector3(0.485f, 0.485f, 0.485f);
+	}
 
     // Update is called once per frame
     void Update()
@@ -29,13 +34,17 @@ public class Movement : MonoBehaviour
         }
         if (isMove)
         {
+
             Vector3 pos = transform.position;
             if (target >= xPosLim) target = xPosLim;
             if (target <= xNegLim) target = xNegLim;
-            pos.x = Mathf.MoveTowards(transform.position.x, target, speed * Time.deltaTime);
+			if (target < transform.position.x) transform.localScale = faceLeft;
+			else if (target > transform.position.x) transform.localScale = faceRight;
+			pos.x = Mathf.MoveTowards(transform.position.x, target, speed * Time.deltaTime);
             transform.position = pos;
         }
-    }
+		anim.SetBool("isMoving", isMove);
+	}
 
     void SetPlayerAvailability()
     {
