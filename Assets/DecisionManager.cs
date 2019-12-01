14b1ba80d9785155a04a701c23ee23ea1d07d5ce
Han <mreyhan693@gmail.com>
2019-12-01 09:31:55 +0700

    Coba

---
+++ Assets/DecisionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool blackmail;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void chooseBlackmail()
+    {
+        blackmail = true;
+    }
+    void chooseBlackmailNot()
+    {
+        blackmail = false;
+    }
+}
